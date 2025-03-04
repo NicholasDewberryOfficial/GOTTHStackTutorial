b6d5eb94d60b83202ce4237a2c8b11b94626026d
Nicholas Dewberry <coolguynickyd@gmail.com>
2025-03-04 13:58:11 -0500

    Right before deploying

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+#https://betterstack.com/community/guides/scaling-go/dockerize-golang/
+# Use Go 1.23 bookworm as base image
+
+FROM golang:1.23.5-bookworm
+
+# Move to working directory /build
+WORKDIR /build
+
+# Copy the go.mod and go.sum files to the /build directory
+COPY go.mod ./
+
+#COPY go.sum ./
+
+# Install dependencies
+RUN go mod download
+
+# Copy the entire source code into the container
+COPY . .
+
+# Build the application
+RUN go build -o /out/dist .
+
+# Start the application
+CMD ["./out/dist"]
+# Document the port that may need to be published
+EXPOSE 8000
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
-	"text/template"
+	"os"
+	"strconv"
 )
 
 func main() {
@@ -10,22 +13,136 @@ func main() {
 	dir := http.Dir("./static")
 	fs := http.FileServer(dir)
 	mux := http.NewServeMux()
-	mux.Handle("/", fs)
-	mux.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("static/index.html")
-		if err != nil {
-			http.Error(w, "oops \n", http.StatusInternalServerError)
-			return
-		}
-		t.Execute(w, nil)
-	})
+	//TODO: USE GLOBS INSTEAD OF REGULAR PARSE FILES (parsefiles is stupidly expensive and is stupid i can do much better)
+	//	statictemplates, err := template.ParseGlob("static/.html")
+	//	slicedtemplates, err := template.ParseGlob("slices/.html")
 
-	mux.HandleFunc("GET /whygotth", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("static/whygotth.html")
+	mux.Handle("/", fs)
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("static/index.html", "slices/genericfooter.html", "slices/genericheader.html")
 		if err != nil {
 			panic(err)
 		}
 		t.Execute(w, nil)
+	})
+	mux.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("static/index.html", "slices/genericfooter.html", "slices/genericheader.html")
+		if err != nil {
+			panic(err)
+		}
+		t.Execute(w, nil)
+	})
+
+	mux.HandleFunc("GET /about", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("static/about.html", "slices/genericfooter.html", "slices/genericheader.html")
+		if err != nil {
+			panic(err)
+		}
+		t.Execute(w, nil)
+
+	})
+
+	mux.HandleFunc("GET /showmewhygoth/{num}", func(w http.ResponseWriter, r *http.Request) {
+
+		tutparam := r.PathValue("num")
+		//fmt.Printf("ID IS : %s", tutparam)
+		if tutparam == "" {
+			http.Redirect(w, r, "/tutorials", http.StatusMovedPermanently)
+		}
+		if tutparam == "tailwindminified.js" {
+			return
+		}
+		intparam, err := strconv.Atoi(tutparam)
+		if err != nil {
+
+			panic(err)
+		}
+
+		parsethisfile := "slices/whatsgotthdetailsslice.html"
+
+		switch intparam {
+		case 1:
+			parsethisfile = "slices/whatsgotthdetailsslice.html"
+		case 2:
+			parsethisfile = "slices/whygotthdetailsslice.html"
+		case 3:
+			parsethisfile = "slices/whyopinionatedgotth.html"
+		case 4:
+			parsethisfile = "slices/whoisthisfor.html"
+		}
+		t, err := template.ParseFiles(parsethisfile)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprint(w, (t.Execute(w, nil)))
+	})
+
+	mux.HandleFunc("GET /whygotth", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("static/whygotth.html", "slices/genericfooter.html", "slices/genericheader.html")
+		if err != nil {
+			panic(err)
+		}
+		t.Execute(w, nil)
+	})
+
+	mux.HandleFunc("GET /tutorials", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("static/tutorials.html", "slices/genericfooter.html", "slices/genericheader.html")
+		if err != nil {
+			panic(err)
+		}
+		t.Execute(w, nil)
+	})
+
+	mux.HandleFunc("GET /0/{id}", func(w http.ResponseWriter, r *http.Request) {
+		tutparam := r.PathValue("id")
+		fmt.Printf("ID IS : %s", tutparam)
+		if tutparam == "" {
+			http.Redirect(w, r, "/tutorials", http.StatusMovedPermanently)
+		}
+		if tutparam == "tailwindminified.js" {
+			return
+		}
+		intparam, err := strconv.Atoi(tutparam)
+		if err != nil {
+
+			panic(err)
+		}
+		parseThisFile := "static/tutorials.html"
+
+		switch intparam {
+		case 0:
+			parseThisFile = "static/noviceintrotogo.html"
+		case 1:
+			parseThisFile = "static/noviceintrotohtmlcss.html"
+		case 2:
+			parseThisFile = "static/novicehtmx.html"
+		case 3:
+			parseThisFile = "static/novicenethttp.html"
+		case 4:
+			//parseThisFile = "static/novice"
+			//TODO: ADD NOVICE TEMPLATING HTML FILE
+		case 5:
+			parseThisFile = "static/novicetailwind.html"
+		}
+
+		t, err := template.ParseFiles(parseThisFile)
+		if err != nil {
+			fmt.Printf(err.Error())
+			return
+		}
+		t.Execute(w, parseThisFile)
+
+	})
+
+	mux.HandleFunc("GET /tailwindminified.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
+		thisfile, err := os.ReadFile("/static/tailwindminified.js")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "", thisfile)
+
 	})
 
 	http.ListenAndServe(":8080", mux)
